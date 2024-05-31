65596bc8d9888aab75e19b7815ea45c0859107e6
Shreya Keshive <shreyakeshive@google.com>
2024-05-31 22:07:20 +0000

    Add unit test

--- cloud_functions/climateiq_data_export_cf/main_test.py
+++ cloud_functions/climateiq_data_export_cf/main_test.py
@@ -287,7 +287,9 @@ def test_export_model_predictions_too_many_predictions(
 
 @mock.patch.object(storage, "Client", autospec=True)
 @mock.patch.object(firestore, "Client", autospec=True)
-def test_export_model_predictions(mock_firestore_client, mock_storage_client) -> None:
+def test_export_model_predictions_h3_centroids_within_chunk(
+    mock_firestore_client, mock_storage_client
+) -> None:
     attributes = {
         "type": "google.cloud.storage.object.v1.finalized",
         "source": "source",
@@ -330,6 +332,73 @@ def test_export_model_predictions(mock_firestore_client, mock_storage_client) ->
             "8d8f2c80c15b83f": 6.0,
             "8d8f2c80c15bc3f": 4.0,
             "8d8f2c80c15bd7f": 5.0,
+        }
+    )
+
+    with pytest.raises(NotImplementedError) as exc_info:
+        main.export_model_predictions(event)
+
+    pd_testing.assert_series_equal(
+        pd.read_json(StringIO(str(exc_info.value)), typ="series"),
+        expected_series,
+        check_dtype=False,
+    )
+
+
+@mock.patch.object(storage, "Client", autospec=True)
+@mock.patch.object(firestore, "Client", autospec=True)
+def test_export_model_predictions_h3_centroids_outside_chunk(
+    mock_firestore_client, mock_storage_client
+) -> None:
+    attributes = {
+        "type": "google.cloud.storage.object.v1.finalized",
+        "source": "source",
+    }
+    data = {
+        "bucket": "climateiq-predictions",
+        "name": "prediction-type/model-id/study-area-name/scenario-id/chunk-id",
+    }
+    event = http.CloudEvent(attributes, data)
+
+    # Build mock Storage object
+    predictions = '{"instance": [1], "prediction": [[1, 2, 3, 4, 5, 6], \
+    [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], \
+    [19, 20, 21, 22, 23, 24]]}\n'
+    with mock_storage_client().bucket("").blob("").open() as mock_fd:
+        mock_fd.__iter__.return_value = iter(predictions.splitlines())
+
+    # Build mock Firestore document
+    metadata = {
+        "name": "study_area_name",
+        "cell_size": 5,
+        "crs": "EPSG:32618",
+        "chunks": {
+            "chunk-id": {
+                "row_count": 4,
+                "col_count": 6,
+                "x_ll_corner": 500,
+                "y_ll_corner": 100,
+            }
+        },
+    }
+    mock_firestore_client().collection().document().get().to_dict.return_value = (
+        metadata
+    )
+
+    # Build expected output data
+    expected_series = pd.Series(
+        {
+            "8d8f2c80c1582bf": 6.0,
+            "8d8f2c80c15863f": 3.0,
+            "8d8f2c80c15867f": 7.5,  # Average of prediction values 4, 5, 10, 11
+            "8d8f2c80c1586bf": 2.0,
+            "8d8f2c80c1586ff": 9.0,
+            "8d8f2c80c15b83f": 23.5,  # Average of prediction values 23, 24
+            "8d8f2c80c15b93f": 15.0,  # Average of prediction values 12, 18
+            "8d8f2c80c15b9bf": 16.5,  # Average of prediction values 16, 17
+            "8d8f2c80c15bc3f": 19.5,  # Average of prediction values 19, 20
+            "8d8f2c80c15bd3f": 11.0,  # Average of prediction values 8, 14
+            "8d8f2c80c15bd7f": 18.0,  # Average of prediction values 15, 21
         }
     )
 
