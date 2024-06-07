38841601df84b4b1a8e61f8d2c895b4985c23e5a
Shreya Keshive <shreyakeshive@google.com>
2024-06-07 16:24:09 +0000

    Squashed commit of the following:
    commit 5028ba5897402e3351ea3cb3584f8c6616b339d2
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Fri Jun 7 16:11:59 2024 +0000
    
        Remove unit test graphs
    
    commit 3e75ae0b356d61d19e3be1a518c64813c306bf83
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Mon Jun 3 16:35:26 2024 +0000
    
        More updates
    
    commit a9d3a989375c2879d3b64b50a9970276bbea744a
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Mon Jun 3 16:34:12 2024 +0000
    
        Re-order imports
    
    commit 90c334f7bfeff10913d20290e347989a9be7ef00
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Mon Jun 3 16:31:18 2024 +0000
    
        Formatting changes
    
    commit 3b852b02ee711baeb8d4952982fa2cd9d9226aa2
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Mon Jun 3 16:30:22 2024 +0000
    
        Updates based on comments
    
    commit 65596bc8d9888aab75e19b7815ea45c0859107e6
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Fri May 31 22:07:20 2024 +0000
    
        Add unit test
    
    commit 13ae049a4811c8e337e9e9db341f85ba0f8210eb
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Fri May 31 19:48:18 2024 +0000
    
        Formatting updates
    
    commit 4e3d66e46e267bab7c6fa68dd80d0f691aa335b7
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Fri May 31 19:44:37 2024 +0000
    
        Fromat fixes
    
    commit d354a8dffc8c56278181cf109da01deb16873d90
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Fri May 31 18:27:27 2024 +0000
    
        Formatting + bug fix
    
    commit 8a84344d69890798a3c7402a5a31a953305b3f18
    Author: Shreya Keshive <shreyakeshive@google.com>
    Date:   Thu May 30 21:08:40 2024 +0000
    
        Add initial h3 logic

--- cloud_functions/climateiq_data_export_cf/main.py
+++ cloud_functions/climateiq_data_export_cf/main.py
@@ -5,11 +5,11 @@ import functions_framework
 import geopandas as gpd
 from google.cloud import firestore
 from google.cloud import storage
+from shapely import geometry
 from h3 import h3
 import pandas as pd
 import pathlib
 import numpy as np
-
 
 GLOBAL_CRS = "EPSG:4326"
 H3_LEVEL = 13
@@ -26,6 +26,7 @@ def export_model_predictions(cloud_event: http.CloudEvent) -> None:
         cloud_event: The CloudEvent representing the storage event. The name
         of the object should conform to the following pattern:
         "<prediction_type>/<model_id>/<study_area_name>/<scenario_id>/<chunk_id>"
+
     Raises:
         ValueError: If the object name format, study area metadata, chunk
         area metadata or predictions file format is invalid.
@@ -47,15 +48,17 @@ def export_model_predictions(cloud_event: http.CloudEvent) -> None:
     study_area_metadata = _get_study_area_metadata(study_area_name)
     chunk_metadata = _get_chunk_metadata(study_area_metadata, chunk_id)
 
-    df = _build_spatialized_model_predictions(
+    spatialized_predictions = _build_spatialized_model_predictions(
         study_area_metadata, chunk_metadata, predictions
     )
 
-    h3_series = _convert_to_h3(df)
+    h3_predictions = _calculate_h3_indexes(
+        study_area_metadata, chunk_metadata, spatialized_predictions
+    )
 
-    # Set the error message to the DataFrame's json string. For testing
-    # purposes. In actual implementation, DataFrame should be stored somewhere.
-    raise NotImplementedError(h3_series.to_json())
+    # Set the error message to the Series' json string for testing
+    # purposes. In the actual implementation, the Series should be stored somewhere.
+    raise NotImplementedError(h3_predictions.to_json())
 
 
 # TODO: Modify this logic once CNN output schema is confirmed. Also update to
@@ -68,8 +71,10 @@ def _read_chunk_predictions(bucket_name: str, object_name: str) -> np.ndarray:
         bucket_name (str): The name of the GCS bucket containing the chunk
         object.
         object_name (str): The name of the chunk object to read.
+
     Returns:
         np.ndarray: A 2D array containing the model predictions for the chunk.
+
     Raises:
         ValueError: If the predictions file format is invalid.
     """
@@ -98,8 +103,10 @@ def _get_study_area_metadata(study_area_name: str) -> dict:
 
     Args:
         study_area_name: The name of the study area to retrieve metadata for.
+
     Returns:
         A dictionary containing metadata for the study area.
+
     Raises:
         ValueError: If the study area does not exist or its metadata is
         missing required fields.
@@ -134,8 +141,10 @@ def _get_chunk_metadata(study_area_metadata: dict, chunk_id: str) -> dict:
         study_area_metadata: A dictionary containing metadata for the
         study area.
         chunk_id: The unique identifier of the chunk to retrieve metadata for.
+
     Returns:
         A dictionary containing metadata for the chunk.
+
     Raises:
         ValueError: If the specified chunk does not exist or its metadata is
         missing required fields.
@@ -171,8 +180,9 @@ def _build_spatialized_model_predictions(
         area.
         chunk_metadata: A dictionary containing metadata for the chunk.
         predictions: A 2D array containing the model predictions for the chunk.
+
     Returns:
-        A DataFrame containing the lat/lon coordinates of cell centers in a
+        A DataFrame containing the lat/lon coordinates of cell centroids in a
         single chunk along with associated predictions, representing a
         subset of the full study area results.
     """
@@ -211,19 +221,102 @@ def _build_spatialized_model_predictions(
     )
 
 
-def _convert_to_h3(df: pd.DataFrame) -> pd.Series:
-    """Builds a Series of h3 indices to the aggregated prediction.
+def _add_h3_index_details(cell: pd.Series) -> pd.Series:
+    """Projects the cell centroid to a H3 index.
 
     Args:
-        df: A pandas DataFrame containing the results of
-        _build_spatialized_model_predictions.
+        cell: A cell row containing the lat and lon of the cell centroid.
+
     Returns:
-        A Series of h3 indices to the aggregated prediction for the lat/lon center
-        points which fall within each h3 cell.
+        A Series containing H3 information for the projected cell centroid.
     """
-    df["h3"] = df.apply(
-        lambda row: h3.geo_to_h3(row["lat"], row["lon"], H3_LEVEL), axis=1
+    h3_index = h3.geo_to_h3(cell["lat"], cell["lon"], H3_LEVEL)
+    centroid_lat, centroid_lon = h3.h3_to_geo(h3_index)
+    boundary_xy = h3.h3_to_geo_boundary(h3_index, True)
+    return pd.Series(
+        {
+            "h3_index": h3_index,
+            "h3_centroid_lat": centroid_lat,
+            "h3_centroid_lon": centroid_lon,
+            "h3_boundary": geometry.Polygon(boundary_xy),
+        }
     )
-    # TODO: Finalize which aggregation to calculate for each prediction type.
-    predictions = df.groupby(["h3"]).prediction.agg("max")
-    return predictions
+
+
+def _get_chunk_boundary(study_area_metadata: dict, chunk_metadata: dict):
+    """Calculates the boundary points of the chunk.
+
+    Args:
+        study_area_metadata: A dictionary containing metadata for the study
+        area.
+        chunk_metadata: A dictionary containing metadata for the chunk.
+
+    Returns:
+        A shapely.Polygon representing the chunk.
+    """
+    min_x = chunk_metadata["x_ll_corner"]
+    min_y = chunk_metadata["y_ll_corner"]
+    max_x = min_x + chunk_metadata["col_count"] * study_area_metadata["cell_size"]
+    max_y = min_y + chunk_metadata["row_count"] * study_area_metadata["cell_size"]
+
+    boundary_points = [
+        (min_x, min_y),
+        (max_x, min_y),
+        (max_x, max_y),
+        (min_x, max_y),
+        (min_x, min_y),
+    ]
+
+    gdf_src_crs = gpd.GeoDataFrame(
+        geometry=[geometry.Polygon(boundary_points)],
+        crs=study_area_metadata["crs"],
+    )
+    gdf_global_crs = gdf_src_crs.to_crs(GLOBAL_CRS)
+    return gdf_global_crs.geometry[0]
+
+
+def _calculate_h3_indexes(
+    study_area_metadata: dict,
+    chunk_metadata: dict,
+    spatialized_predictions: pd.DataFrame,
+) -> pd.Series:
+    """Projects cell centroids to H3 indexes.
+
+    Filters out cells that have projected H3 centroids outside of the current
+    chunk and de-dupes duplicate H3 projections by using the average prediction
+    value from overlapping cells.
+
+    Args:
+        study_area_metadata: A dictionary containing metadata for the study
+        area.
+        chunk_metadata: A dictionary containing metadata for the chunk.
+        spatialized_predictions: A DataFrame containing the lat/lon coordinates of cell
+        centroids in a single chunk along with associated predictions
+
+    Returns:
+        A Series containing H3 indexes in a single chunk along with associated
+        predictions, representing a subset of the full study area results.
+    """
+    # Calculate H3 information for each cell.
+    spatialized_predictions[
+        ["h3_index", "h3_centroid_lat", "h3_centroid_lon", "h3_boundary"]
+    ] = spatialized_predictions.apply(_add_h3_index_details, axis=1)
+
+    # Filter out any cells where the projected H3 centroid falls outside of the
+    # chunk boundary.
+    chunk_boundary = _get_chunk_boundary(study_area_metadata, chunk_metadata)
+    spatialized_predictions = spatialized_predictions[
+        spatialized_predictions.apply(
+            lambda cell: chunk_boundary.contains(
+                geometry.Point(cell["h3_centroid_lon"], cell["h3_centroid_lat"])
+            ),
+            axis=1,
+        )
+    ]
+
+    # TODO: Add logic for fetching predictions from neighboring chunks for boundary
+    # cells.
+    spatialized_predictions = spatialized_predictions.groupby(
+        ["h3_index"]
+    ).prediction.agg("mean")
+    return spatialized_predictions
