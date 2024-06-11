8b61ea9e7044d35f83b30672e8535bbcc9902762
Shreya Keshive <shreyakeshive@google.com>
2024-06-11 20:39:15 +0000

    Add updated tests

--- cloud_functions/climateiq_data_export_cf/main_test.py
+++ cloud_functions/climateiq_data_export_cf/main_test.py
@@ -356,10 +356,9 @@ def test_export_model_predictions_missing_expected_neighbor_chunk(
         metadata
     )
 
-    # Neighbor chunks do not exist.
-    mock_firestore_client().collection().document().collection().where().where(). \
-        limit().get().exists = (
-            False
+    # Build neighbor chunk data.
+    mock_firestore_client().collection().document().collection().where().where().limit().get().exists = (
+        False  # Neighbor chunks do not exist.
     )
 
     with pytest.raises(ValueError) as exc_info:
@@ -413,20 +412,22 @@ def test_export_model_predictions_invalid_neighbor_chunk(
         metadata
     )
 
+    # Build neighbor chunk data.
     neighbor_metadata = metadata["chunks"]["chunk-id"].copy()
-    neighbor_metadata.pop("row_count")
-    mock_firestore_client().collection().document().collection().where().where(). \
-        limit().get().to_dict.return_value = (
-            neighbor_metadata
+    neighbor_metadata["id"] = "neighbor-chunk-id"
+    neighbor_metadata.pop("row_count")  # Missing "row_count" required field
+    mock_firestore_client().collection().document().collection().where().where().limit().get().to_dict.return_value = (
+        neighbor_metadata
     )
 
     with pytest.raises(ValueError) as exc_info:
         main.export_model_predictions(event)
 
-    assert (
-        "Neighbor chunk at index" in str(exc_info.value)
-        and " is missing one or more required fields: id, row_count, col_count,"
-        " x_ll_corner,y_ll_corner, x_index, y_index" in str(exc_info.value))
+    assert "Neighbor chunk at index" in str(
+        exc_info.value
+    ) and " is missing one or more required fields: id, row_count, col_count," " x_ll_corner,y_ll_corner, x_index, y_index" in str(
+        exc_info.value
+    )
 
 
 @mock.patch.object(storage, "Client", autospec=True)
@@ -447,7 +448,9 @@ def test_export_model_predictions_neighbor_chunk_missing_predictions(
     # Build mock Storage object
     predictions = '{"instance": [1], "prediction": [[1, 2, 3], [4, 5, 6]]}\n'
     with mock_storage_client().bucket("").blob("").open() as mock_fd:
-        mock_fd.__iter__.return_value = iter(predictions.splitlines())
+        mock_fd.__iter__.return_value = iter(
+            predictions.splitlines()
+        )  # Predictions for current chunk only
 
     # Build mock Firestore document
     metadata = {
@@ -472,11 +475,11 @@ def test_export_model_predictions_neighbor_chunk_missing_predictions(
         metadata
     )
 
+    # Build neighbor chunk data.
     neighbor_metadata = metadata["chunks"]["chunk-id"].copy()
     neighbor_metadata["id"] = "neighbor-chunk-id"
-    mock_firestore_client().collection().document().collection().where().where(). \
-        limit().get().to_dict.return_value = (
-            neighbor_metadata
+    mock_firestore_client().collection().document().collection().where().where().limit().get().to_dict.return_value = (
+        neighbor_metadata
     )
 
     with pytest.raises(ValueError) as exc_info:
@@ -513,7 +516,7 @@ def test_export_model_predictions_h3_centroids_within_chunk(
             iter(predictions.splitlines()),
             iter(predictions.splitlines()),
             iter(predictions.splitlines()),
-        ]
+        ]  # Current chunk + 5 valid neighbors: {(0, 1), (2, 1), (0, 0), (2, 0), (1, 0)}
 
     # Build mock Firestore document
     metadata = {
@@ -538,21 +541,21 @@ def test_export_model_predictions_h3_centroids_within_chunk(
         metadata
     )
 
+    # Build neighbor chunk data.
     neighbor_metadata = metadata["chunks"]["chunk-id"].copy()
     neighbor_metadata["id"] = "neighbor-chunk-id"
-    mock_firestore_client().collection().document().collection().where().where(). \
-        limit().get().to_dict.return_value = (
-            neighbor_metadata
+    mock_firestore_client().collection().document().collection().where().where().limit().get().to_dict.return_value = (
+        neighbor_metadata
     )
 
-    # Build expected output data
+    # Build expected output data (neighbor chunks have same data as current chunk in this test so prediction values stay the same after aggregation.)
     expected_series = pd.Series(
         {
-            "8d8f2c80c1582bf": 3.0,  # Boundary H3 cell (top, right, upper right)
-            "8d8f2c80c1586bf": 1.0,  # Boundary H3 cell (top)
+            "8d8f2c80c1582bf": 3.0,
+            "8d8f2c80c1586bf": 1.0,
             "8d8f2c80c1586ff": 2.0,
-            "8d8f2c80c15b83f": 6.0,  # Boundary H3 cell (bottom, right)
-            "8d8f2c80c15bc3f": 4.0,  # Boundary H3 cell (bottom)
+            "8d8f2c80c15b83f": 6.0,
+            "8d8f2c80c15bc3f": 4.0,
             "8d8f2c80c15bd7f": 5.0,
         }
     )
@@ -586,6 +589,7 @@ def test_export_model_predictions_h3_centroids_outside_chunk(
     predictions = '{"instance": [1], "prediction": [[1, 2, 3, 4, 5, 6], \
     [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], \
     [19, 20, 21, 22, 23, 24]]}\n'
+
     with mock_storage_client().bucket("").blob("").open() as mock_fd:
         mock_fd.__iter__.side_effect = [
             iter(predictions.splitlines()),
@@ -594,7 +598,7 @@ def test_export_model_predictions_h3_centroids_outside_chunk(
             iter(predictions.splitlines()),
             iter(predictions.splitlines()),
             iter(predictions.splitlines()),
-        ]
+        ]  # Current chunk + 5 valid neighbors: {(0, 1), (2, 1), (0, 0), (2, 0), (1, 0)}
 
     # Build mock Firestore document
     metadata = {
@@ -619,27 +623,149 @@ def test_export_model_predictions_h3_centroids_outside_chunk(
         metadata
     )
 
+    # Build neighbor chunk data.
     neighbor_metadata = metadata["chunks"]["chunk-id"].copy()
     neighbor_metadata["id"] = "neighbor-chunk-id"
-    mock_firestore_client().collection().document().collection().where().where(). \
-        limit().get().to_dict.return_value = (
-            neighbor_metadata
+    mock_firestore_client().collection().document().collection().where().where().limit().get().to_dict.return_value = (
+        neighbor_metadata
     )
 
-    # Build expected output data
+    # Build expected output data (neighbor chunks have same data as current chunk in this test so prediction values stay the same after aggregation.)
     expected_series = pd.Series(
         {
             "8d8f2c80c1582bf": 6.0,
             "8d8f2c80c15863f": 3.0,
-            "8d8f2c80c15867f": 7.5,  # Average of prediction values 4, 5, 10, 11
+            "8d8f2c80c15867f": 7.5,  
             "8d8f2c80c1586bf": 2.0,
             "8d8f2c80c1586ff": 9.0,
-            "8d8f2c80c15b83f": 23.5,  # Average of prediction values 23, 24
-            "8d8f2c80c15b93f": 15.0,  # Average of prediction values 12, 18
-            "8d8f2c80c15b9bf": 16.5,  # Average of prediction values 16, 17
-            "8d8f2c80c15bc3f": 19.5,  # Average of prediction values 19, 20
-            "8d8f2c80c15bd3f": 11.0,  # Average of prediction values 8, 14
-            "8d8f2c80c15bd7f": 18.0,  # Average of prediction values 15, 21
+            "8d8f2c80c15b83f": 23.5,  # Average of prediction values 23, 24 (from current chunk)
+            "8d8f2c80c15b93f": 15.0,  # Average of prediction values 12, 18 (from current chunk)
+            "8d8f2c80c15b9bf": 16.5,  # Average of prediction values 16, 17 (from current chunk)
+            "8d8f2c80c15bc3f": 19.5,  # Average of prediction values 19, 20 (from current chunk)
+            "8d8f2c80c15bd3f": 11.0,  # Average of prediction values 8, 14 (from current chunk)
+            "8d8f2c80c15bd7f": 18.0,  # Average of prediction values 15, 21 (from current chunk)
+        }
+    )
+
+    with pytest.raises(NotImplementedError) as exc_info:
+        main.export_model_predictions(event)
+
+    pd_testing.assert_series_equal(
+        pd.read_json(StringIO(str(exc_info.value)), typ="series"),
+        expected_series,
+        check_dtype=False,
+    )
+
+
+@mock.patch.object(storage, "Client", autospec=True)
+@mock.patch.object(firestore, "Client", autospec=True)
+def test_export_model_predictions_overlapping_neighbors(
+    mock_firestore_client, mock_storage_client
+) -> None:
+    attributes = {
+        "type": "google.cloud.storage.object.v1.finalized",
+        "source": "source",
+    }
+    data = {
+        "bucket": "climateiq-predictions",
+        "name": "prediction-type/model-id/study-area-name/scenario-id/chunk-id",
+    }
+    event = http.CloudEvent(attributes, data)
+
+    # Build mock Storage object
+    predictions = '{"instance": [1], "prediction": [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30]]}\n'
+    predictions_bottom = '{"instance": [1], "prediction": [[31, 32, 33, 34, 35, 36], [37, 38, 39, 40, 41, 42], [43, 44, 45, 46, 47, 48], [49, 50, 51, 52, 53, 54], [55, 56, 57, 58, 59, 60]]}\n'
+
+    with mock_storage_client().bucket("").blob("").open() as mock_fd:
+        mock_fd.__iter__.side_effect = [
+            iter(predictions.splitlines()),
+            iter(predictions_bottom.splitlines()),
+        ]  # Current chunk + 1 intersecting neighbor
+
+    # Build mock Firestore document
+    metadata = {
+        "name": "study_area_name",
+        "cell_size": 3,
+        "crs": "EPSG:32618",
+        "row_count": 2,
+        "col_count": 3,
+        "chunks": {
+            "chunk-id": {
+                "id": "chunk-id",
+                "row_count": 5,
+                "col_count": 6,
+                "x_ll_corner": 500,
+                "y_ll_corner": 100,
+                "x_index": 1,
+                "y_index": 1,
+            }
+        },
+    }
+    mock_firestore_client().collection().document().get().to_dict.return_value = (
+        metadata
+    )
+
+    # Build neighbor chunk data.
+    neighbor_left = {
+        "id": "neighbor-chunk-left",
+        "row_count": 5,
+        "col_count": 6,
+        "x_ll_corner": 482,
+        "y_ll_corner": 100,
+        "x_index": 0,
+        "y_index": 1,
+    }
+    neighbor_right = {
+        "id": "neighbor-chunk-right",
+        "row_count": 5,
+        "col_count": 6,
+        "x_ll_corner": 518,
+        "y_ll_corner": 100,
+        "x_index": 2,
+        "y_index": 1,
+    }
+    neighbor_bottom_left = {
+        "id": "neighbor-chunk-bottom-left",
+        "row_count": 5,
+        "col_count": 6,
+        "x_ll_corner": 482,
+        "y_ll_corner": 85,
+        "x_index": 0,
+        "y_index": 0,
+    }
+    neighbor_bottom_right = {
+        "id": "neighbor-chunk-bottom-right",
+        "row_count": 5,
+        "col_count": 6,
+        "x_ll_corner": 518,
+        "y_ll_corner": 85,
+        "x_index": 2,
+        "y_index": 0,
+    }
+    neighbor_bottom = {
+        "id": "neighbor-chunk-bottom",
+        "row_count": 5,
+        "col_count": 6,
+        "x_ll_corner": 500,
+        "y_ll_corner": 85,
+        "x_index": 1,
+        "y_index": 0,
+    }
+    mock_firestore_client().collection().document().collection().where().where().limit().get().to_dict.side_effect = [
+        neighbor_left,
+        neighbor_right,
+        neighbor_bottom_left,
+        neighbor_bottom_right,
+        neighbor_bottom,
+    ]
+
+    # Build expected output data
+    expected_series = pd.Series(
+        {
+            "8d8f2c80c1586ff": 8.0, # Average of prediction values 10, 11, 12, 4, 5, 6 (from current chunk)
+            "8d8f2c80c15bc3f": 26.2857142857, # Average of prediction values 25, 26, 27, 20, 21, (from current chunk) and 32, 33 (from bottom neighbor chunk)
+            "8d8f2c80c15bd3f": 11.5, # Average of prediction values  14, 15, 8, 9 (from current chunk)
+            "8d8f2c80c15bd7f": 22.714285714, # Average of prediction values 28, 29, 22, 23, 24, 16, 17 (from current chunk)
         }
     )
 
